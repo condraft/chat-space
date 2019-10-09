d5c9dd47cb007018506777723cc4493ff23c5cc4
condraft <condraft64@gmail.com>
2019-10-09 13:44:33 +0900

    writejs

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="chat-main__messages__message" data-id="${message.id}">
+                  <div class="chat-main__messages__message__upper-info">
+                    <p class="chat-main__messages__message__upper-info__talker">
+                      ${message.user_name}
+                    </p>
+                    <p class="chat-main__messages__message__upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="chat-main__messages__message__text">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                 </div>`
+  return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +32,13 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+    })
+    .fail(function(data){
+      alert('エラーが発生しました')
+    })
   })
-})
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,8 +10,9 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.html{ redirect_to group_messages_path(@group)}, notice: 'メッセージが送信されました'
+        format.html{ redirect_to group_messages_path(@group),notice: 'メッセージが送信されました'}
         format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
@@ -29,5 +30,4 @@ class MessagesController < ApplicationController
   def set_group
     @group = Group.find(params[:group_id])
   end
-  
 end
